617acfd80dedc751318a294b74cdc39b05b4f9dc
Iryna Chyrkova <irenchik.fart@gmail.com>
2025-02-05 15:24:19 +0200

    First and second part of homework 4

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Lesson4 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        //квадрат числа
+        System.out.println("Введіть ціле число: ");
+        int number = new Scanner(System.in).nextInt();
+
+        int squareNumber = number*number;
+        System.out.println("Квадрат числа " + number + " дорівнює " + squareNumber + ".");
+        System.out.println("\n");
+
+        //об"єм циліндра
+        double cylinderRadius = 3.5;
+        double cylinderHeight = 10.0;
+        double V = Math.PI*cylinderRadius*cylinderRadius*cylinderHeight;
+        System.out.println(V);
+
+
+
     }
 }

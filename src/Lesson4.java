525a5fd4731c02eda4668ee496311fa0466dc9e3
Iryna Chyrkova <irenchik.fart@gmail.com>
2025-02-10 12:11:43 +0200

    all 3 tasks with methods

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -6,21 +6,36 @@ public class Lesson4 {
         //квадрат числа
         System.out.println("Введіть ціле число: ");
         int number = new Scanner(System.in).nextInt();
-        int squareNumber = number*number;
-        System.out.println("1) Квадрат числа " + number + " дорівнює " + squareNumber + ".\n");
+        System.out.println("1) Квадрат числа " + number + " дорівнює " + squareNumber(number) + ".\n");
 
         //об"єм циліндра
-        double cylinderRadius = 3.5;
-        double cylinderHeight = 10.0;
-        double V = Math.PI*cylinderRadius*cylinderRadius*cylinderHeight;
-        System.out.println("2) Об'єм циліндра з радіусом " + cylinderRadius + " і висотою " + cylinderHeight + " дорівнює "  + V + ".\n");
+        double radius = Double.parseDouble(args[0]);
+        double height = Double.parseDouble(args[1]);
+        System.out.println("2) Об'єм циліндра з радіусом " + radius + " і висотою " + height + " дорівнює " + volume(radius,height) + ".\n");
 
         //число в ступені
         System.out.println("3) Введіть число а: ");
         int mainNumber = new Scanner(System.in).nextInt();
         System.out.println("   Введіть число b: ");
         int powerOfNumber = new Scanner(System.in).nextInt();
-        long result = (long)Math.pow(mainNumber, powerOfNumber);
-        System.out.println("   Результат " + mainNumber + "^" + powerOfNumber + " дорівнює "  + result + ".");
+        System.out.println("   Результат " + mainNumber + "^" + powerOfNumber + " дорівнює "  + calculation (mainNumber, powerOfNumber) + ".");
+
     }
+
+
+    static int squareNumber(int number){
+        int result= number*number;
+        return result;
+    }
+
+    static double volume(double cylinderRadius, double cylinderHeight){
+        double cylinderVolume = Math.PI*cylinderRadius*cylinderRadius*cylinderHeight;
+        return cylinderVolume;
+    }
+
+    static int calculation (int mainNumber, int powerOfNumber){
+        int result = (int)Math.pow(mainNumber, powerOfNumber);
+        return result;
+    }
+    
 }

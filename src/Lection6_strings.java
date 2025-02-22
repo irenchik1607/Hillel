5574d18017401478a2730cc9402481203722ee65
Iryna Chyrkova <irenchik.fart@gmail.com>
2025-02-22 17:19:09 +0200

    string 7.1

--- src/Lection6_strings.java
+++ src/Lection6_strings.java
@@ -1,29 +1,35 @@
 import java.util.Scanner;
 
 public class Lection6_strings {
-    public static void main(String[] args) {
+    public static void main() {
 
 
         Scanner scanner = new Scanner(System.in);
 
-        String word = "Hello";
+        //ДЗ 7.1. Рядок, створений з n повторень останніх n символів
+        System.out.println("Введи строку");
+        String word = scanner.nextLine();
         int length = word.length();
-        System.out.println("Введіть значення від 1 до " + length);
-        int number = scanner.nextInt();
-       // System.out.println(subWordMethod(number, length, word));
+        if (length == 0) {
+            System.out.println("Міша, ти не ввів строку");
+        } else {
+            System.out.println("Введіть значення від 1 до " + length);
+            int number = scanner.nextInt();
+            System.out.println(subWordMethod(number, length, word));
+        }
     }
 
+    //ДЗ 7.1. Рядок, створений з n повторень останніх n символів
     static String subWordMethod(int number, int length, String word) {
         String subWord = new String();
         if (number >= 1 && number <= length) {
 
             for (int i = 1; i <= number; i++) {
-                subWord = word.substring(length-number);
-           System.out.println(subWord);
+                subWord = word.substring(length - number).repeat(number);
             }
+            return subWord;
         } else {
-            System.out.println("Міша, ти вводиш некоректне значення");
+            return "Міша, ти вводиш некоректне значення";
         }
-        return subWord;
     }
 }

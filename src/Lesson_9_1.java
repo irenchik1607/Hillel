a57c32938c8dab28ddff98d36a7b4d28ccc8916f
Iryna Chyrkova <irenchik.fart@gmail.com>
2025-02-28 01:29:01 +0200

    9.1

---
+++ src/Lesson_9_1.java
@@ -0,0 +1,8 @@
+public class Lesson_9_1 {
+    public static void main(String[] args) {
+
+        Person firstPerson = new Person();
+        Person secondPerson = new Person();
+        Person thirdPerson = new Person();
+    }
+}

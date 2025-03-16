309424e19b6547b2e902ae5084990c11aa91fbca
Iryna Chyrkova <irenchik.fart@gmail.com>
2025-03-16 21:29:07 +0200

    13.1

---
+++ src/Lesson_13/Main.java
@@ -0,0 +1,21 @@
+package Lesson_13;
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] stringValues = {"12aqa34", "tiredLife","eva","eva"};
+        System.out.println(getLengthOfStrings(stringValues));
+    }
+
+    public  static Map<String, Integer> getLengthOfStrings(String[] stringValues){
+        Map<String, Integer> lengthOfStrings = new LinkedHashMap<>();
+        for(String s:stringValues){
+            lengthOfStrings.put(s, s.length());
+        } return lengthOfStrings;
+    }
+}

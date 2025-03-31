5d94978dad804a9cb15bcf8b8bc06f823412151d
Iryna Chyrkova <irenchik.fart@gmail.com>
2025-03-31 14:40:59 +0300

    home work 17

--- src/Main/Lesson_17/Main.java
+++ src/Main/Lesson_17/Main.java
@@ -5,15 +5,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        WebDriver browser = new ChromeDriver();
 
-    WebDriver browser = new ChromeDriver();
+        try {
+            // Runtime.getRuntime().addShutdownHook(new Thread(()->browser.quit()));
 
-   // Runtime.getRuntime().addShutdownHook(new Thread(()->browser.quit()));
+            browser.get("https://guest:welcome2qauto@qauto.forstudy.space/");
+            WebElement guestLogIn = browser.findElement(By.cssSelector("button.-guest"));
+            guestLogIn.click();
 
-    browser.get("https://guest:welcome2qauto@qauto.forstudy.space/");
-    WebElement guestLogIn = browser.findElement(By.cssSelector("button.-guest"));
-        guestLogIn.click();
+
+            List<WebElement> groupOfElements = browser.findElements(By.cssSelector("a.sidebar_btn"));
+            for (WebElement group : groupOfElements) {
+                System.out.println(group.getText());
+            }
+        } catch (Exception e) {
+            System.out.println("was exeption" + e.getMessage());
+            ;
+        } finally {
+            browser.quit();
+        }
+
     }
+
 }

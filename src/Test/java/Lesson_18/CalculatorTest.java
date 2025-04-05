af4b3620c9a51bc2cdda233d2e2b455092fcb97b
Iryna Chyrkova <irenchik.fart@gmail.com>
2025-04-05 18:09:33 +0300

    home work 18

--- src/Test/java/Lesson_18/CalculatorTest.java
+++ src/Test/java/Lesson_18/CalculatorTest.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-//import java.Lesson_18.Calculator;
+
 
 public class CalculatorTest {
 

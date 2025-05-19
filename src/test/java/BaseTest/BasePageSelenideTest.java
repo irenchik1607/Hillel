fd9845ebfb5c6a66e8ec836caa89d2227d598971
Iryna Chyrkova <irenchik.fart@gmail.com>
2025-05-19 23:58:53 +0300

    home work 29

--- src/test/java/BaseTest/BasePageSelenideTest.java
+++ src/test/java/BaseTest/BasePageSelenideTest.java
@@ -11,7 +11,8 @@ public class BasePageSelenideTest {
     @BeforeMethod
     public void setUp() {
         Configuration.pollingInterval = 100;
-        Configuration.remote = "http://localhost:8080/";
+//        Configuration.remote = "http://localhost:8080/";
+        Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browser = browser;
        // Configuration.pageLoadStrategy = "eager"; //шукає завантаження потрібного елементу
         Selenide.open("https://guest:welcome2qauto@qauto.forstudy.space/");

1b32fe6673e4d5953d32b99bfaffd2e43c77e8bc
Iryna Chyrkova <irenchik.fart@gmail.com>
2025-05-22 23:26:40 +0300

    home work 31

--- src/main/java/Lesson_31/RestApiMethods.java
+++
@@ -1,7 +0,0 @@
-package Lesson_31;
-import io.restassured.RestAssured;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
-public class RestApiMethods {
-}
